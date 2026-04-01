8a1da14345973099fa8c08a4aa465be0aa605838
akhilakunuku <151796070+akhilakunuku@users.noreply.github.com>
2026-04-01 15:34:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Render 🚀!"
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
